9415dc31a07c9e29f4a462a8c1070ad6ee241c56
Raffi <raffi.bedoyan@azumo.co>
2024-02-19 18:20:39 -0500

    Update build and postbuild scripts, and fix ngrok image in docker-compose.yml

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const dotenv = require('dotenv')
 const webpack = require('webpack');
-
 
 module.exports = (env, argv) => {
   const envPath = env.ENVIRONMENT ? `.env.${env.ENVIRONMENT}` : '.env';
   
   console.log('[WEBPACK]', env.ENVIRONMENT, envPath);
+
+  dotenv.config({ path: envPath }); 
   
   const config = {
     entry: './public/content.js',
@@ -17,14 +18,7 @@ module.exports = (env, argv) => {
     },
     plugins: [
       new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(env.ENVIRONMENT),
-        },
-      }),
-        new Dotenv({
-              safe: false,
-              path: "./" + envPath
-          })
+        'process.env': JSON.stringify(process.env)})
       ]
   };
 
